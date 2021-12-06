6496cb4a9e5929230781c071c023f9f853978e61
MihajloRa <mihajlorasajkovski@gmail.com>
2021-12-06 23:53:10 +0100

    Domashna 2

--- Домашна 2/Tech_Prototype/src/main/java/com/example/tech_prototype/Service/impl/AttractionServiceImpl.java
+++ Домашна 2/Tech_Prototype/src/main/java/com/example/tech_prototype/Service/impl/AttractionServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.tech_prototype.Service.impl;
 
+import com.example.tech_prototype.Model.Point;
 import com.example.tech_prototype.Repository.PointRepository;
 import com.example.tech_prototype.Service.AttractionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AttractionServiceImpl implements AttractionService {
@@ -15,5 +18,5 @@ public class AttractionServiceImpl implements AttractionService {
     }
 
     @Override
-    public List<Point> getPoints() { return this.repository.findAllBy}
+    public List<Point> getPoints() { return this.repository.findAll(); }
 }
