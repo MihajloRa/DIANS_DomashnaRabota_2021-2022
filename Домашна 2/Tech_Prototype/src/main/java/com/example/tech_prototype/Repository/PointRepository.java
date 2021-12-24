19613a74367a0d3b9fde41c4f2967c220ed724d4
MihajloRa <mihajlorasajkovski@gmail.com>
2021-12-24 01:26:13 +0100

    Domasn 3 - Start

--- Домашна 2/Tech_Prototype/src/main/java/com/example/tech_prototype/Repository/PointRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tech_prototype.Repository;
-
-import com.example.tech_prototype.Model.Point;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PointRepository extends JpaRepository<Point, Integer> {
-}
